d0d6ab7f42c80dc2933b7e13daf30c8c46b94396
santy432 <santiagoleonespina@gmail.com>
2026-09-22 16:17:10 -0300

    Qeuueueueueeu en el juego por NaAnTe Juana aporto con TikTok
    Chupete en orto inside 🐷

--- Assets/Scripts/dataStructures/EnemySpawnerManager.cs
+++ Assets/Scripts/dataStructures/EnemySpawnerManager.cs
@@ -47,6 +47,7 @@ public class EnemySpawnerManager : MonoBehaviour
         for (int i = 0; i < enemyOrder.Length; i++)
         {
             enemyQueue.Enqueue(enemyOrder[i]);
+            Debug.Log($"Enqueued enemy: {enemyOrder[i].name}");
         }
     }
 
